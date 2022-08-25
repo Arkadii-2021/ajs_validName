d28203943b7bced7b5cd7b6957f46140c37ca710
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-08-25 18:06:19 +0700

    fix regexp string

--- src/js/validName.js
+++ src/js/validName.js
@@ -4,7 +4,7 @@ export default class Validator {
   }
 
   validateUsername() {
-    const regex = /[^\d\W_](([a-z-_])\d{0,3})*[^\d\W_]/i;
+    const regex = /[^\d\W_](^\d)*(([^a-z|\D])\d{0,3})*[^\d\W_]\w*/i;
     if (this.nick.match(regex)[0] === this.nick.match(regex).input) {
       return true;
     }
